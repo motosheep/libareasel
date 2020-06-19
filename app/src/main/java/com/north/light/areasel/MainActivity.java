fee1796083ab278110e48c2b3d68d44592faad5a
li <2487177596@qq.com>
2020-06-19 09:28:02 +0800

    1微调

--- app/src/main/java/com/north/light/areasel/MainActivity.java
+++ app/src/main/java/com/north/light/areasel/MainActivity.java
@@ -1,8 +1,8 @@
 package com.north.light.areasel;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
